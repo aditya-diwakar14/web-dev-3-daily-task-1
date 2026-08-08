e35515319ef25c5f92ba04ae0b635aeee7697454
aditya-diwakar14 <adityadiwakar1437@gmail.com>
2026-08-08 22:18:16 +0530

    Add files via upload

--- day 1/app.js
+++ day 1/app.js
@@ -11,5 +11,12 @@
 // const user = os.userInfo();
 // console.log(user);
 
-const des = require('./data/des');
-console.log(des);
+// const des = require('./data/des');
+// console.log(des);
+
+// const process = require('process');
+// console.log(process.argv[3]);
+
+require('dotenv').config()
+const process = require('process');
+console.log(process.env.PORT);
